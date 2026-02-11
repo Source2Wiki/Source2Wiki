54363817b8cb94b7b655181c53059ac7d8b27269
Angel <angelcazacu8@gmail.com>
2026-02-11 17:45:44 +0100

    fix

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -24,7 +24,6 @@ export default function Img({ src, alt, ...props }: ImgProps) {
     <div style={{ 
       display: 'flex', 
       justifyContent: 'left',
-      margin: '1rem 0'
     }}>
       <img 
         {...props}
@@ -32,9 +31,8 @@ export default function Img({ src, alt, ...props }: ImgProps) {
         alt={displayAlt}
         style={{ 
           maxWidth,
-          width: '100%',
+          width: 'auto',
           height: 'auto',
-          borderRadius: '8px',
         }} 
       />
     </div>
--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -3,11 +3,13 @@ import {Tool} from '@site/src/components/Software';
 import {Social} from '@site/src/components/Software';
 import DateRender from '@site/src/components/DateRenderer';
 import OriginalMDXComponents from '@theme-original/MDXComponents';
+import Img from '@site/src/components/Img';
 
 export default {
   ...OriginalMDXComponents,
   Game,
   Tool,
   Social,
+  img: Img,
   DateRender
 };
