ca0a809989901bfe018a23286a7c03d9e612cf80
Angel <angelcazacu8@gmail.com>
2026-08-16 18:59:48 +0200

    throw on broken markdown links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -108,7 +108,7 @@ const config: Config = {
   ],
   markdown: {
     hooks: {
-      onBrokenMarkdownLinks: 'warn',
+      onBrokenMarkdownLinks: 'throw',
       onBrokenMarkdownImages: 'throw',
     },
   },
